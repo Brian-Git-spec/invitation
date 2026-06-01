7b18f7d3f36c1549e2bfc56aec75db0eb57decc7
Brian-Git-spec <brianwairagi@gmail.com>
2026-06-01 17:03:23 +0300

    fix:debugging

--- graduation-app/frontend/src/services/api.ts
+++ graduation-app/frontend/src/services/api.ts
@@ -4,6 +4,8 @@ const API_URL = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api` 
   : 'http://127.0.0.1:5000/api';
 
+  console.log("API_URL =", API_URL);
+
 export const sendInvitation = async (name: string, relationship: string) => {
     const response = await axios.post(`${API_URL}/invite`, { name, relationship });
     return response.data;
